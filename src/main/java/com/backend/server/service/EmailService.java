5b17565c9546f736623158619bdcf5d5e128cab7
halim <halim.choukani@gmail.com>
2024-11-08 13:15:02 +0100

    fix email sender message

--- src/main/java/com/backend/server/service/EmailService.java
+++ src/main/java/com/backend/server/service/EmailService.java
@@ -1,20 +1,26 @@
 package com.backend.server.service;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailService {
+
     @Autowired
     private JavaMailSender mailSender;
 
-    public void sendEmail(String to, String subject, String body) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(to);
-        message.setSubject(subject);
-        message.setText(body);
+    public void sendEmail(String to, String subject, String body) throws MessagingException {
+        MimeMessage message = mailSender.createMimeMessage();
+
+        // Use MimeMessageHelper to set HTML content
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setText(body, true); // Set "true" for HTML content
 
         mailSender.send(message);
     }
